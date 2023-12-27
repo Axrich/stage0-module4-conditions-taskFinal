e684cec029610f00689e33c7424095b9009602e7
Axrich <saidahror0101@icloud.com>
2023-12-27 23:08:22 +0500

    src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,34 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.time.YearMonth;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0) {
+            System.out.println("Invalid date");
+            return;
+        }
 
+        try {
+            YearMonth yearMonth = YearMonth.of(year, month);
+            int daysInMonth = yearMonth.lengthOfMonth();
+
+            if (yearMonth.isLeapYear()) {
+                System.out.println("Leap year!");
+            }
+
+            System.out.println("Number of days in the month: " + daysInMonth);
+        } catch (Exception e) {
+            System.out.println("Invalid date");
+        }
+    }
+
+    public static void main(String[] args) {
+        DaysInMonth daysInMonth = new DaysInMonth();
+
+        daysInMonth.printDays(2023, 2);
+        daysInMonth.printDays(2024, 2);
+        daysInMonth.printDays(2023, 13);
+        daysInMonth.printDays(-2023, 7);
     }
 }
