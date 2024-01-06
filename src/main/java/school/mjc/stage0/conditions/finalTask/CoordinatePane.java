d2867a62278d66c0c15be44ae8e824c462738029
Axrich <saidahror0101@icloud.com>
2024-01-06 22:43:40 +0500

    message

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x > 0 && y > 0) {
+            System.out.println("First quadrant");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Second quadrant");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Third quadrant");
+        } else if (x > 0 && y < 0) {
+            System.out.println("Fourth quadrant");
+        } else if (x == 0 && y == 0) {
+            System.out.println("Zero point");
+        }
+    }
+
+    public static void main(String[] args) {
+        CoordinatePane coordinatePane = new CoordinatePane();
+
+        coordinatePane.printQuadrant(3, 4);
+        coordinatePane.printQuadrant(-2, 5);
+        coordinatePane.printQuadrant(-3, -2);
+        coordinatePane.printQuadrant(4, -1);
+        coordinatePane.printQuadrant(0, 0);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        try {
+            int result = dividend / divider;
+
+            int product = result * divider;
+
+            if (product == dividend) {
+                System.out.println("Can be divided completely");
+            } else {
+                System.out.println("Cannot be divided completely");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        IntegerDivider divider = new IntegerDivider();
+
+        divider.printCompletelyDivided(10, 2);
+        divider.printCompletelyDivided(15, 3);
+        divider.printCompletelyDivided(7, 2);
+        divider.printCompletelyDivided(10, 0);
     }
 }
